5f84814bc1db2a54aebaff57dc4c6bcf298100a1
Jason Franklin <jjfrankl@ncsu.edu>
2024-12-19 21:17:18 -0500

    making sure it is running

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { } from '@angular/common';
+// import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
